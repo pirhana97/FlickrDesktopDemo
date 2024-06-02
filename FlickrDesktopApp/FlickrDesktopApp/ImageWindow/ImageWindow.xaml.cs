758a1e32266eb09a0a9e5344c6b28ce3788e5fd8
pirhana97 <40688374+pirhana97@users.noreply.github.com>
2024-06-02 15:46:41 +0530

    Add files via upload

---
+++ FlickrDesktopApp/FlickrDesktopApp/ImageWindow/ImageWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FlickrDesktopApp
+{
+    /// <summary>   
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ImageWindow : Window
+    {
+        //Empty Constructor
+        public ImageWindow()
+        {
+
+        }
+
+        //Depedency Injection in the parameterized constructor 
+        public ImageWindow(ImageSource imageSource)
+        {
+            InitializeComponent();
+            DisplayedImage.Source = imageSource;
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            //Capture the mouse wheel event 
+            base.OnMouseWheel(e);
+
+            // Zoom in or out
+            if (e.Delta > 0)
+            {
+                var transform = (ScaleTransform)DisplayedImage.RenderTransform;
+                transform.ScaleX *= 1.1;
+                transform.ScaleY *= 1.1;
+            }
+            else if (e.Delta < 0)
+            {
+                var transform = (ScaleTransform)DisplayedImage.RenderTransform;
+                transform.ScaleX /= 1.1;
+                transform.ScaleY /= 1.1;
+            }
+        }
+
+    }
+
+
+}
---
+++ FlickrDesktopApp/FlickrDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Unsplasharp;
+
+namespace FlickrDesktopApp
+{
+    /// <summary>   
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<ImageModel> Images { get; set; }
+        private string AccessKey = "T7oOFF-VuXqwWVRD06TULiWXUHpDqthhdNgAJLDNDMM";
+        private int photosPerPage = 12;
+        private int page = 1;
+
+
+        public MainWindow()
+        {
+            //Components from xaml are initalized
+            InitializeComponent();
+            Images = new ObservableCollection<ImageModel>();
+        }
+
+        private async void photoSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //LoadInitialImages method is invoke when photoSearch_Click event is subscribed 
+            await LoadInitialImages();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Get the source of the image
+            var imageSource = ((Image)sender).Source;
+
+            // Create and show the new window
+            var imageWindow = new ImageWindow(imageSource);
+            imageWindow.Show();
+        }
+
+        private async void loadMorePhotos_Click(object sender, RoutedEventArgs e)
+        {
+            // Load more images when the end of the scroll is reached
+            await LoadMoreImages();
+        }
+
+        private async Task LoadInitialImages()
+        {
+            //Page is reset to 1 when new query is passed in the input
+            page=1;
+
+            //Previous result is cleared when a new input query is added
+            Images.Clear();
+            await LoadImages();
+        }
+
+        private async Task LoadImages()
+        {
+            var photosToSearch = inputQuery.Text;
+            var client = new UnsplasharpClient(AccessKey);
+
+            //var photoUrl = await client.SearchPhotos(photosToSearch); // Also works
+
+            string url = string.Format("{0}?query={1}&page={2}&per_page={3}&client_id={4}", "https://api.unsplash.com/search/photos", photosToSearch, page, photosPerPage, AccessKey);
+            var photoUrl = await client.FetchSearchPhotosList(url);
+
+            for (int i = 0; i < photosPerPage; i++)
+            {
+                var imagePath = photoUrl[i].Urls.Regular;
+                Images.Add(new ImageModel { ImageUrl = imagePath });
+            }
+
+            photoListView.ItemsSource = Images;
+        }
+
+        private async Task LoadMoreImages()
+        {
+            page++;
+            await LoadImages();
+        }
+    }
+
+    public class ImageModel
+    {
+        public string ImageUrl { get; set; }
+    }
+}
